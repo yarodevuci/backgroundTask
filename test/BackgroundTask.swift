7e646f7f9def0a3f8d31530c3431637798edee5f
Yaroslav Dukal <dukalyar@gmail.com>
2016-08-28 00:18:54 -0700

    Added handling for interrupted audio case minor fixes

--- test/BackgroundTask.swift
+++ test/BackgroundTask.swift
@@ -1,6 +1,5 @@
 //
 //  BackgroundTask.swift
-//  test
 //
 //  Created by Yaro on 8/27/16.
 //  Copyright © 2016 Yaro. All rights reserved.
@@ -11,33 +10,38 @@ import AVFoundation
 class BackgroundTask {
     
     var player = AVAudioPlayer()
-   
+    var timer = NSTimer()
+    
     func startBackgroundTask() {
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(interuptedAudio), name: AVAudioSessionInterruptionNotification, object: AVAudioSession.sharedInstance())
         self.playAudio()
     }
     
     func stopBackgroundTask() {
+        NSNotificationCenter.defaultCenter().removeObserver(self, name: AVAudioSessionInterruptionNotification, object: nil)
         player.stop()
     }
     
+    @objc private func interuptedAudio(notification: NSNotification) {
+        if notification.name == AVAudioSessionInterruptionNotification && notification.userInfo != nil {
+            var info = notification.userInfo!
+            var intValue = 0
+            info[AVAudioSessionInterruptionTypeKey]!.getValue(&intValue)
+            if intValue == 1 { playAudio() }
+        }
+    }
     
-    func playAudio() {
-        dispatch_async(dispatch_get_main_queue(), {() -> Void in
-            do {
-                if let bundle = NSBundle.mainBundle().pathForResource("3", ofType: "wav") {
-                    let alertSound = NSURL(fileURLWithPath: bundle)
-                    try AVAudioSession.sharedInstance().setCategory(AVAudioSessionCategoryPlayback, withOptions:AVAudioSessionCategoryOptions.MixWithOthers)
-                    try AVAudioSession.sharedInstance().setActive(true)
-                    try self.player = AVAudioPlayer(contentsOfURL: alertSound)
-                    self.player.numberOfLoops = -1
-                    self.player.volume = 0.01
-                    self.player.prepareToPlay()
-                    self.player.play()
-                    
-                }
-            } catch {
-                print(error)
-            }
-        })
+    private func playAudio() {
+        do {
+            let bundle = NSBundle.mainBundle().pathForResource("11", ofType: "wav")
+            let alertSound = NSURL(fileURLWithPath: bundle!)
+            try AVAudioSession.sharedInstance().setCategory(AVAudioSessionCategoryPlayback, withOptions:AVAudioSessionCategoryOptions.MixWithOthers)
+            try AVAudioSession.sharedInstance().setActive(true)
+            try self.player = AVAudioPlayer(contentsOfURL: alertSound)
+            self.player.numberOfLoops = -1
+            self.player.volume = 0.01
+            self.player.prepareToPlay()
+            self.player.play()
+        } catch { print(error) }
     }
 }
